d6af686b48bba6aa99c3bdee0298461a560afd0d
Tamim Al Hasan <50096476+talhasan06@users.noreply.github.com>
2023-11-04 05:52:54 +0600

    again last check.py

--- final.py
+++ final.py
@@ -1,129 +1,119 @@
 import random
-class User:
-    def __init__(self,name , email, address, account_type, bank) -> None:
+
+class BankingSystem:
+    def __init__(self):
+        self.accounts={}
+        self.allow_loans=True
+
+    def create_account(self,name,email,address,account_type):
+        account = Account(name,email,address,account_type)
+        self.accounts[account.account_number]=account
+        return account
+    
+    def delete_account(self,account_number):
+        for account in  self.accounts:
+            if account.account_number == account_number:
+                self.accounts.remove(account)
+                print(f"Account {account_number} deleted")
+                return
+            print(f"Account {account_number} not found")
+    
+    def list_accounts(self):
+        for account_number,account  in self.accounts.items():
+            print(account_number, account.name)
+
+    def total_balance(self):
+        total=0
+        for account in self.accounts.values():
+            total+=account.balance
+        return total
+    
+    def total_loans(self):
+        total=0
+        for account in self.accounts.values():
+            total += account.num_loans
+        return total
+    
+        
+    def print_accounts(self):
+        print(f"Total accounts: {len(self.accounts)}")
+        print(f"Total balance: {self.total_balance()}")
+        print(f"Total loans: {self.total_loans()}")
+
+    # def is_loan_available(self):
+
+    def print_all_users(self):
+        for account in self.accounts.values():
+            print(f"Account Number: {account.account_number}")
+            print(f"Name: {account.name}")
+            print(f"Email: {account.email}")
+            print(f"Address: {account.address}")
+            print(f"Account Type: {account.account_type}")
+            print(f"Balance: {account.balance}")
+            print(f"Number of Loans: {account.num_loans}")
+            print(f"Allow Loans: {'Yes' if account.allow_loans else 'No'}")
+            print("--------------------------")
+
+class Account:
+    def __init__(self,name,email,address,account_type):
         self.name=name
         self.email=email
         self.address=address
         self.account_type=account_type
-        self.account_no=self.generate_account_number()
         self.balance=0
-        self.bank=bank
-        self.statement=[]
-
-    def generate_account_number(self):
-        return str(random.randint(10**10, 10**11 - 1))
+        self.account_number = random.randint(100000,999999)
+        self.transaction_history=[]
+        self.num_loans=0
 
     def deposit(self,amount):
-        if amount > 0:
-            self.balance += amount
-            self.bank.bank_total_balance += amount
-            print("---------------")
-            print("Deposit Done")
-            print("---------------")
-            self.statement.append(f"Deposit: ${amount}")
-        else:
-            print("Negative amount not accepted")
+        self.balance += amount
+        self.transaction_history.append(f"Deposited {amount}")
+        print("**** Deposit successful ****")
 
     def withdraw(self,amount):
-        if amount > 0:
-            if (amount > self.balance):
-                print("Withdraw amount exceeded")
-            else:
-                self.balance-=amount
-                self.bank.bank_total_balance -= amount
-                print("---------------")
-                print("Withdraw successful")
-                print("---------------")
-                self.statement.append(f"Withdraw: ${amount}")
+        if amount > self.balance:
+            print("Withdraw amount exceeded")
         else:
-            print("Negative amount not accepted")
-
-    def available_balance(self):
-        print("---------------")
-        print(f"{self.name} Available Balance: {self.balance}")
-        print("---------------")
-
-    def transfer(self,user_email,amount):
-        if amount> 0:
-            if (amount <= self.balance):
-                for user in self.bank.users:
-                    if user.email==user_email:
-                        self.balance -= amount
-                        user.balance += amount
-                        self.statement.append(f"Transfer ${amount} to a/c no:{user.account_no} successful :) ")
-                else:
-                    print("Account does not exist")
-            else:
-                print("Your balance is too low to transfer")
+            self.balance -= amount
+            self.transaction_history.append(f"Withdraw {amount}")
+            print("**** Withdraw successful ****")
+        
+    def take_loan(self, amount):
+        if banking_system.allow_loans and self.num_loans < 2:
+            self.balance += amount
+            self.num_loans += 1
+            self.transaction_history.append(f"Took loan of {amount}")
+            print("**** Loan request accepted ****")
+        elif not self.allow_loans:
+            print("Loans are not allow for this account")
         else:
-            print("Negative amount not accepted")
-    
-    def loan_request(self,amount):
-            if amount > 0 and len(self.bank.loans)<2:
-                self.bank.loans[self]=amount
-                self.balance += amount
-                self.bank.total_loan_amount += amount
-                print("---------------")
-                print("Loan request accepted")
-                print("---------------")
-                self.statement.append(f"---Received a loan of ${amount}---")
-            elif len(self.bank.loans)>=2:
-                print("-------You already cross the limit of maximum number of loans------")
-            else:
-                # print("--------Loan request not accepted--------")
-                print("---------------")
-                print("Loan request not accepted ,Sorry !!!")
-                print("---------------")
-    def view_statement(self):
-        for stat in self.statement:
-            print(stat)
+            print("Can only take 2 loans")
 
-class Bank:
-    def __init__(self):
-        self.users=[]
-        self.loans={}
-        self.is_bankrupt=False
-        self.is_loan_enabled=True
-        self.bank_total_balance=0
-        self.total_loan_amount=0
-
-    def create_user(self,name,email,address,acount_type):
-        user=User(name,email,address,acount_type,self)
-        self.users.append(user)
-        print("user created successfully: ",user.name)
-        print("-----------------------------")
-        return user
-
-    def delete_user(self,user_email):
-        for user in self.users:
-            if user.email==user_email:
-                self.users.remove(user)
-                print("---------------")
-                print("User deleted successfully")
-                print("---------------")
+    def transfer(self,receiver,amount):
+        if receiver.account_number not in banking_system.accounts:
+                print("Account does not exists")
         else:
-            print("User not found")
+            if amount > self.balance:
+                print("Insufficient balance")
+            else:
+                self.balance -= amount
+                receiver.balance += amount
+                self.transaction_history.append(f"Transferred {amount} to account {receiver.account_number}")
+                receiver.transaction_history.append(f"Received {amount} from account {self.account_number}")
+                print("**** Transfer successful ****")
 
-    def total_balance(self):
-        balance=sum(user.balance for user in self.users)
-        print(balance)
+    def print_statement(self):
+        print(f"Name: {self.name}") 
+        print(f"Email: {self.email}")
+        print(f"Address: {self.address}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Account Type: {self.account_type}")
+        print(f"Balance: {self.balance}")
+        print("Transaction History:")
+        for transaction in self.transaction_history:
+            print(transaction)
 
-    def total_loan(self):
-        loan=sum(self.loans.values())
-        print(loan)
-
-    def print_users(self):
-        for user in self.users:
-            print(f"{user.name} {user.email} {user.address} {user.account_type} {user.account_no}" )
-            print("-----------------------------")
-    
-    def toggle_loan(self,enable):
-        self.is_loan_enabled=enable
-
-admin = Bank()
-
-# user1.transfer(user2,500)
-
+banking_system = BankingSystem()
 
 print("1.CREATE AN ACCOUNT")
 print("2.DEPOSIT")
@@ -132,92 +122,128 @@ print("4.CHECK BALANCE")
 print("5.TRANSACTION HISTORY")
 print("6.APPLY FOR LOAN")
 print("7.BALANCE TRANSFER")
-print("8.ADMIN PANEL")
+print("8.Admin")
 print("9.Exit")
-
+print("--------------------------")
 choice=int(input("Enter your choice:"))
-print("-----------------------------")
-while(choice!=9):
 
-    if(choice==1):
+while(choice!=9):
+    if (choice==1):
         name=input("Enter name:")
         email=input('Enter your email:')
         address=input("Enter your address:")
         account_type=input("Account type:")
-        user = admin.create_user(name,email,address,account_type)
+        user = banking_system.create_account(name,email,address,account_type)
+        print(f"Here is your account number:{user.account_number}")
+        print("--------------------------")
     elif(choice==2):
+        account_number=int(input("Enter account number you want to deposit:"))
         deposit_amount=int(input('Enter amount to deposit:'))
-        user.deposit(deposit_amount)
-    elif(choice==3):
-        withdraw_amount=int(input('Enter withdraw amount:'))
-        user.withdraw(withdraw_amount)
-    elif(choice==4):
-        user.available_balance()
-    elif(choice==5):
-        user.view_statement()
-    elif(choice==6):
-        if(user.bank.is_loan_enabled):
-            loan_amount=int(input('Enter loan amount:'))
-            user.loan_request(loan_amount)
+        if account_number in banking_system.accounts:
+            account=banking_system.accounts[account_number]
+            account.deposit(deposit_amount)
+            print("--------------------------")
         else:
-            print("Currently loan system OFF :( ")
-            print("-----------------------------")
-    elif(choice==7):
-        transfer_amount=int(input('Enter amount to transfer:'))
-        transfer_email=input("Enter email you want to transfer this amount:")
-        user.transfer(transfer_email,transfer_amount)
-    elif(choice==8): #admin panel
-        print("------Admin Panel-------")
-        print("1.CREATE AN ACCOUNT")
-        print("2.DELETE AN ACCOUNT")
-        print("3.SEE ACCOUNT LIST")
-        print("4.BANK AVAILABLE BALANCE")
-        print("5.TOTAL LOAN AMOUNT")
-        print("6.LOAN ON OFF")
-        print("7.Exit")
-        
-        choice=int(input("Enter your choice:"))
-        print("-----------------------------")
+            print("Account not found")
+            print("--------------------------")
+    elif choice == 3:
+        account_number = int(input("Enter account number you want to withdraw from:"))
+        withdraw_amount = int(input('Enter amount to withdraw:'))
+        if account_number in banking_system.accounts:
+            account = banking_system.accounts[account_number]
+            account.withdraw(withdraw_amount)
+            print("--------------------------")
+        else:
+            print("Account not found")
+            print("--------------------------")
+    elif(choice==4):
+        account_number = int(input("Enter account number you want to check balance for:"))
+        if account_number in banking_system.accounts:
+            account = banking_system.accounts[account_number]
+            print(account.name,account.balance)
+            print("--------------------------")
+        else:
+            print("Account not found")
+            print("--------------------------")
 
-        while(choice!=7):
+    elif choice == 5:
+        account_number = int(input("Enter account number to view transaction history:"))
+        if account_number in banking_system.accounts:
+            account = banking_system.accounts[account_number]
+            account.print_statement()
+            print("--------------------------")
+        else:
+            print("Account not found")
+            print("--------------------------")
+            
+    elif choice == 6:
+        account_number = int(input("Enter account number to apply for a loan:"))
+        loan_amount = int(input('Enter the loan amount:'))
+        if account_number in banking_system.accounts:
+            account = banking_system.accounts[account_number]
+            account.take_loan(loan_amount)
+            print("--------------------------")
+        else:
+            print("Account not found")
+            print("--------------------------")
+    elif choice == 7:
+        sender_account_number = int(input("Enter your account number:"))
+        receiver_account_number = int(input("Enter the receiver account number:"))
+        transfer_amount = int(input('Enter the amount to transfer:'))
+        if sender_account_number in banking_system.accounts and receiver_account_number in banking_system.accounts:
+            sender = banking_system.accounts[sender_account_number]
+            receiver = banking_system.accounts[receiver_account_number]
+            sender.transfer(receiver, transfer_amount)
+            print("--------------------------")
+        else:
+            print("Account not found")
+            print("--------------------------")
+    
+    elif choice == 8:
+        password = input("Enter password:")
+        default_pass="1A2bB345"
+        if(password == default_pass):
+            print("1.CREATE AN ACCOUNT")
+            print("2.DELETE AN ACCOUNT")
+            print("3.ACCOUNTS LIST")
+            print("4.CHECK TOTAL BALANCE OF THE BANK")
+            print("5.CHECK TOTAL LOAN AMOUNT")
+            print("6.LOAN FEATURE OF THE BANK")
+
+            choice = int(input("Enter your choice:"))
 
             if(choice==1):
                 name=input("Enter name:")
                 email=input('Enter your email:')
                 address=input("Enter your address:")
                 account_type=input("Account type:")
-                user = admin.create_user(name,email,address,account_type)
+                user = banking_system.create_account(name,email,address,account_type)
+                print(f"Here is your account number:{user.account_number}")
+                print("--------------------------")
             elif(choice==2):
-                delete_email=input("Enter email to delete your account:")
-                admin.delete_user(delete_email)
-            elif(choice==3):
-                admin.print_users()
-            elif(choice==4):
-                print("---------------")
-                print("Total Balance: ",admin.bank_total_balance)
-                print("---------------")
-            elif(choice==5):
-                print(admin.total_loan_amount)
-            elif(choice==6):
-                loan_on_off=input('Enter loan On or Off:').lower()
-                print("-----------------------------")
-                if(loan_on_off=="on"):
-                    admin.toggle_loan(True)
-                elif(loan_on_off=="off"):
-                    admin.toggle_loan(False)
+                account_number=int(input("Enter account number to DELETE:"))
+                if account_number in banking_system.accounts:
+                    account=banking_system.accounts[account_number]
+                    account.delete_account(account_number)
+                    print("--------------------------")
+                else:
+                    print("Account not found")
+                    print("--------------------------")
+            elif (choice==3):
+                    banking_system.print_all_users()
+            elif (choice==4):
+                    print(f"Total Balance: ${banking_system.total_balance()}")
+            elif (choice==5):
+                    print(f"Total Loan: ${banking_system.total_loans()}")
+            elif (choice==6):
+                print(f"Loan Feature Active: {'Yes' if banking_system.allow_loans else 'No'}")
+                user_input=input("Stop Loan feature press Y :")
+                upper_input=user_input.upper()
+                if(upper_input=="Y"):
+                      
 
-            print("1.CREATE AN ACCOUNT")
-            print("2.DELETE AN ACCOUNT")
-            print("3.SEE ACCOUNT LIST")
-            print("4.BANK AVAILABLE BALANCE")
-            print("5.TOTAL LOAN AMOUNT")
-            print("6.LOAN ON OFF")
-            print("7.LOGOUT FROM ADMIN PANEL")
-            
-            choice=int(input("Enter your choice:"))
-            print("-----------------------------")
-            # admin panel end
 
+    
     print("1.CREATE AN ACCOUNT")
     print("2.DEPOSIT")
     print("3.WITHDRAW")
@@ -225,9 +251,7 @@ while(choice!=9):
     print("5.TRANSACTION HISTORY")
     print("6.APPLY FOR LOAN")
     print("7.BALANCE TRANSFER")
-    print("8.ADMIN PANEL")
+    print("8.Print all User")
     print("9.Exit")
-
-    choice=int(input("Enter your choice:"))
-    print("-----------------------------")
-    
+    print("--------------------------")
+    choice = int(input("Enter your choice:"))
